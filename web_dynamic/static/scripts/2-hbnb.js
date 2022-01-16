4d8844168d4878fcbfdb2676c18f7382624f1fdb
RockoGilbert <evolve2122@gmail.com>
2022-01-15 18:54:51 -0600

    Added a path to remove  the class available to the div#api_status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,11 @@
 $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
   if (data.status === 'OK') {
     $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
   }
+});
+
 $(function() {
   const $amenitiesCheck = {};
   $('input[type=checkbox]').change(function() {
