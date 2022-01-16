a6a8a14ba8cee4aa1b450acb37955fa25035b66f
RockoGilbert <evolve2122@gmail.com>
2022-01-16 08:07:13 -0600

    Redid the join method used on amenities h4

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,45 +1,12 @@
 $(function() {
-  const $amenitiesCheck = {};
+  const $amenityCheck = {};
   $('input[type=checkbox]').change(function() {
     if ($(this).is(':checked')) {
-      const key = $(this).attr('data-id');
-      const value = $(this).attr('data-name');
-      $amenitiesCheck[key]= value;
-    }
-    if (!$(this).is(':checked')) {
-      delete amenitiesCheck[$(this).attr('data-id')];
-    }
-    const list = [];
-    for (const name in amenitiesCheck) {
-      list.push(amenitiesCheck[name]);
-    }
-    $('div.amenities h4').text(list.join(', '));
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready (function() {
-  const dict = {};
-  const $amenitiesCheck = $('input[type-checkbox]');
-  const $selectedAmenities = $('div.amenities h4');
-    
-  $amenitiesCheck.click(function() {
-    if ($(this).is(':checked')) {
-      dict[$(this).data('id')] = $(this).data('name');
-      $selectedAmenities.text(Object.values(dict).join(', '));
+      amenityCheck[$(this).attr('data-name')] = $(this).attr('data-id');
     } else if ($(this).is(':not(:checked)')) {
-      delete dict[$(this).data('id')];
-      $selectedAmenities.text(Object.values(dict).join(', '));
+      delete amenityCheck[$(this).attr('data-name')];
     }
+    const names = Object.keys(amenityObj);
+    $('.amenities h4').text(names.sort().join(', '));
   });
-});
+})
