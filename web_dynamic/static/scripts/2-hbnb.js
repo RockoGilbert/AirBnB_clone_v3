1a0e7531b079e55f1dbff8c135b29e5185f49657
RockoGilbert <evolve2122@gmail.com>
2022-01-16 14:05:40 -0600

    Added api function to check and see if it's connected and to add and remove a class depending on availability

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,26 +1,30 @@
-$.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
-  if (data.status === 'OK') {
-    $('#api_status').addClass('available');
-  } else {
-    $('#api_status').removeClass('available');
-  }
-});
+$(document).ready(rock);
 
-$(function() {
-  const $amenitiesCheck = {};
+const HOST = '0.0.0.0';
+
+function rock () {
+  const $amenityCheck = {};
   $('input[type=checkbox]').change(function() {
     if ($(this).is(':checked')) {
-      const key = $(this).attr('data-id');
-      const value = $(this).attr('data-name');
-      $amenitiesCheck[key]= value;
+      amenityCheck[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityCheck[$(this).attr('data-name')];
     }
-    if (!$(this).is(':checked')) {
-      delete amenitiesCheck[$(this).attr('data-id')];
-    }
-    const list = [];
-    for (const name in amenitiesCheck) {
-      list.push(amenitiesCheck[name]);
-    }
-    $('div.amenities h4').text(list.join(', '));
+    const names = Object.keys(amenityCheck);
+    $('.amenities h4').text(names.sort().join(', '));
   });
-});
+
+
+apiStatus();
+}
+
+function apiStatus () {
+  const API_URL = 'http://${HOST}:5001/api/v1/status/`';
+  $.get(API_URL, (data, testStatus) => {
+    if (textStatus === 'success' && data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+}
