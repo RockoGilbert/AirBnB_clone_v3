98c7e3016a879c153863af4295a5393ab4a4422c
RockoGilbert <evolve2122@gmail.com>
2022-01-15 18:04:31 -0600

    Added a path to delete if it's not checked.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,33 @@
+$(function() {
+  const $amenitiesCheck = {};
+  $('input[type=checkbox]').change(function() {
+    if ($(this).is(':checked')) {
+      const key = $(this).attr('data-id');
+      const value = $(this).attr('data-name');
+      $amenitiesCheck[key]= value;
+    }
+    if (!$(this).is(':checked')) {
+      delete amenitiesCheck[$(this).attr('data-id')];
+    }
+    const list = [];
+    for (const name in amenitiesCheck) {
+      list.push(amenitiesCheck[name]);
+    }
+    $('div.amenities h4').text(list.join(', '));
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready (function() {
   const dict = {};
   const $amenitiesCheck = $('input[type-checkbox]');
