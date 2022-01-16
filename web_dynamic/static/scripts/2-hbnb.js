e92047d3900ef6f77282c713b30d8c4528c16469
RockoGilbert <evolve2122@gmail.com>
2022-01-15 18:11:50 -0600

    Added a path to get the api and check if the status is ok.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,22 @@
 $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
   if (data.status === 'OK') {
-    $('DIV#api_status').addClass('available');
+    $('#api_status').addClass('available');
   }
+$(function() {
+  const $amenitiesCheck = {};
+  $('input[type=checkbox]').change(function() {
+    if ($(this).is(':checked')) {
+      const key = $(this).attr('data-id');
+      const value = $(this).attr('data-name');
+      $amenitiesCheck[key]= value;
+    }
+    if (!$(this).is(':checked')) {
+      delete amenitiesCheck[$(this).attr('data-id')];
+    }
+    const list = [];
+    for (const name in amenitiesCheck) {
+      list.push(amenitiesCheck[name]);
+    }
+    $('div.amenities h4').text(list.join(', '));
+  });
 });
